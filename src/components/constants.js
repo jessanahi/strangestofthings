bb8adf4c0fd27e0ac1c4428dc38e3196189aeaf2
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-24 20:38:21 -0600

    dummy styling

---
+++ src/components/constants.js
@@ -0,0 +1,9 @@
+export const REGISTER_URL = 'https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-PT/users/register';
+
+export const LOGIN_URL = 'https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-PT/users/login';
+
+export const POSTS_URL = 'https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-PT/posts';
+
+export const USER_URL = 'https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-PT/users/me';
+
+export const TOKEN_STORAGE_KEY = 'strange_token';
