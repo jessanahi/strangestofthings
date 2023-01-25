cbcbb5e2e25cdc43ff7dcb461f47264c3d637e7d
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-24 20:19:40 -0600

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Posts from './components/Posts';
+import Navibar from './components/Navibar';
 
 function App() {
   return (
     <div className="App">
-
+      <Navibar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='posts' element={<Posts />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navibar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navibar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/profile'>Profile</a>
+          </li>
+          <li>
+            <a href='/posts'>Posts</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navibar;
